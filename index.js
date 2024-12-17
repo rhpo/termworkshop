11e2ee35323441ee4f5db6a594948e70fca93eb2
RHPO <ramyhadid2005.rh@gmail.com>
2024-12-17 19:42:31 +0100

    Sec Ver

--- index.js
+++ index.js
@@ -3,114 +3,125 @@ const input_el = document.getElementById("prompt");
 const output_el = document.getElementById("output");
 
 var state = {
-    x: 0,
-    y: 1,
-    pi: Math.PI
+	x: 0,
+	y: 1,
+	pi: Math.PI
 }
 
 function message(text, isError) {
-    var p = document.createElement('p');
-    p.innerText = text + '.';
+	var p = document.createElement('p');
+	p.innerText = text + '.';
 
-    if (isError) {
-        p.style.color = 'red'
-    }
+	if (isError) {
+		p.style.color = 'crimson'
+	}
 
-    output_el?.appendChild(p);
+	output_el?.appendChild(p);
 
-    output_el.scrollTo({
-        top: output_el.scrollHeight
-    })
+	output_el.scrollTo({
+		top: output_el.scrollHeight
+	})
 }
 
-function handle_keyup(event) {
-    if (event.key === 'Enter') {
-        var text = input_el.value;
-        var words = text.split(' ');
+function handle_keypress(event) {
 
-        var command = words.shift();
-        var arguments = words;
+	if (event.key == 'Enter') {
+		var text = input_el.value;
 
-        input_el.value = '' // we don't need the input anymore...
+		if (text.length === 0)
+			return message('YOu SuCk.', true);
 
-        switch (command) {
-            case "echo":
-                return handleEcho(command, arguments)
+		var words = text.split(' ');
+		var command = words.shift();
+		var arguments = words;
 
-            case "assign":
-                return handleAssignment(command, arguments);
+		input_el.value = '' // we don't need the input anymore...
 
-            case "add":
-            case "sub":
-            case "mul":
-            case "div":
-                return handleBinary(command, arguments);
+		switch (command) {
+			case "echo":
+				return handleEcho(command, arguments)
 
-            case "display":
-                return handleDisplay(command, arguments)
+			case "display":
+				return handleDisplay(command, arguments);
 
-            default:
-                message('command not found', true)
-                break;
-        }
+			case "clear":
+				return handleClear(command, arguments);
 
-    }
+			case "add":
+			case "sub":
+			case "mul":
+			case "div":
+				return handleBinary(command, arguments);
+
+			default:
+				message(`${command}: command not found`, true)
+				break;
+		}
+
+	}
 }
 
 function handleEcho(command, arguments) {
-    // print the arguments
-    message(arguments.join(' '));
+	// print the arguments
+	message(arguments.join(' '));
+}
+
+function handleClear(command, arguments) {
+	// remove all paragraphs inside the output
+	output_el.innerHTML = ''
+
+	message('Console is cleared.')
 }
 
 function handleDisplay(command, arguments) {
-    // print the arguments
-    alert(arguments.join(' '));
+	// print the arguments
+	alert(arguments.join(' '));
 }
 
 function handleBinary(command, arguments) {
 
-    var first = parseFloat(arguments[0]);
-    var second = parseFloat(arguments[1]);
+	var first = parseFloat(arguments[0]);
+	var second = parseFloat(arguments[1]);
 
-    var result;
-    switch (command) {
-        case "add":
-            result = first + second;
-            message(`= ${result}`);
-            break;
+	var result;
+	switch (command) {
+		case "add":
+			result = first + second;
+			message(`= ${result}`);
+			break;
 
-        case "sub":
-            result = first - second;
-            message(`= ${result}`);
-            break;
+		case "sub":
+			result = first - second;
+			message(`= ${result}`);
+			break;
 
-        case "mul":
-            result = first * second;
-            message(`= ${result}`);
-            break;
+		case "mul":
+			result = first * second;
+			message(`= ${result}`);
+			break;
 
-        case "div":
-            result = first / second;
-            message(`= ${result}`);
-            break;
-    }
+		case "div":
+			result = first / second;
+			message(`= ${result}`);
+			break;
+	}
 }
 
 function isIdentifier(t) {
-    return /a-z/.test(t);
+	return /a-z/.test(t);
 }
 
 function handleAssignment(command, arguments) {
 
-    if (arguments.length > 2) {
-        return message('You entered too many arguments', true);
-    }
+	if (arguments.length > 2) {
+		return message('You entered too many arguments', true);
+	}
 
-    var variable = arguments[0];
-    var value = arguments[1];
-    state[variable] = value;
+	var variable = arguments[0];
+	var value = arguments[1];
+	state[variable] = value;
 
-    message(`${variable} = ${value}`);
+	message(`${variable} = ${value}`);
 }
 
-input_el?.addEventListener('keyup', handle_keyup);
+input_el?.addEventListener('keypress', handle_keypress);
